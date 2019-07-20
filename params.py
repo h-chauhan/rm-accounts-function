5ade7d3d35f4e9c761a7a814c25be6ad0eb99e29
Himanshu Chauhan <hchauhan1404@outlook.com>
2019-07-20 14:57:36 +0530

    Updated year

--- params.py
+++ params.py
@@ -3,11 +3,11 @@ getParams = lambda type: {
     'username_field': 'intern_student_username_rollnumber',
     'password_field': 'intern_student_password',
     'notifsUrl': 'http://tnp.dtu.ac.in/rm_2016-17/intern/intern_student',
-    'year': '2K16'
+    'year': '2K17'
 } if type == 'internship' else {
     'loginUrl': 'http://tnp.dtu.ac.in/rm_2016-17/login',
     'username_field': 'student_username_rollnumber',
     'password_field': 'student_password',
     'notifsUrl': 'http://tnp.dtu.ac.in/rm_2016-17/student',
-    'year': '2K15'
+    'year': '2K16'
 }
